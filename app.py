7d34543ca279276cb17bc4a4c4588749de4d6ff5
Johann <han8@gmx.de>
2024-10-21 13:16:57 +0200

    Setup database and insert sample data

--- app.py
+++ app.py
@@ -1,108 +1,45 @@
-from flask import Flask, render_template, redirect, url_for, request, abort, flash
+import os
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap5
-import forms
+from db import db  # Import the db instance
+from models import User  # Import the User model
 
 app = Flask(__name__)
 
+# Configuration
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config.from_mapping(
-    SECRET_KEY = 'secret_key_just_for_dev_environment',
-    BOOTSTRAP_BOOTSWATCH_THEME = 'pulse'
+    SECRET_KEY='secret_key_just_for_dev_environment',
+    BOOTSTRAP_BOOTSWATCH_THEME='pulse',
+    SQLALCHEMY_DATABASE_URI=f'sqlite:///{os.path.join(basedir, "instance", "automotive_parts.sqlite")}',
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
 )
 
-from db import db, Todo, List, insert_sample  # (1.)
+# Initialize the database with the app
+db.init_app(app)
+Bootstrap5(app)
 
-bootstrap = Bootstrap5(app)
+# Create tables if they don't exist
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return redirect(url_for('todos'))
-
-@app.route('/todos/', methods=['GET', 'POST'])
-def todos():
-    form = forms.CreateTodoForm()
-    if request.method == 'GET':
-        todos = db.session.execute(db.select(Todo).order_by(Todo.id)).scalars()  # !!
-        return render_template('todos.html', todos=todos, form=form)
-    else:  # request.method == 'POST'
-        if form.validate():
-            todo = Todo(description=form.description.data)  # !!
-            db.session.add(todo)  # !!
-            db.session.commit()  # !!
-            flash('Todo has been created.', 'success')
-        else:
-            flash('No todo creation: validation error.', 'warning')
-        return redirect(url_for('todos'))
-
-@app.route('/todos/<int:id>', methods=['GET', 'POST'])
-def todo(id):
-    todo = db.session.get(Todo, id)  # !!
-    form = forms.TodoForm(obj=todo)  # (2.)  # !!
-    if request.method == 'GET':
-        if todo:
-            if todo.lists: form.list_id.data = todo.lists[0].id  # (3.)  # !!
-            choices = db.session.execute(db.select(List).order_by(List.name)).scalars()  # !!
-            form.list_id.choices = [(0, 'List?')] + [(c.id, c.name) for c in choices]  # !!
-            return render_template('todo.html', form=form)
-        else:
-            abort(404)
-    else:  # request.method == 'POST'
-        if form.method.data == 'PATCH':
-            if form.validate():
-                form.populate_obj(todo)  # (4.)
-                todo.populate_lists([form.list_id.data])  # (5.)  # !!
-                db.session.add(todo)  # !!
-                db.session.commit()  # !!
-                flash('Todo has been updated.', 'success')
-            else:
-                flash('No todo update: validation error.', 'warning')
-            return redirect(url_for('todo', id=id))
-        elif form.method.data == 'DELETE':
-            db.session.delete(todo)  # !!
-            db.session.commit()  # !!
-            flash('Todo has been deleted.', 'success')
-            return redirect(url_for('todos'), 303)
-        else:
-            flash('Nothing happened.', 'info')
-            return redirect(url_for('todo', id=id))
-
-@app.route('/lists/')
-def lists():
-    lists = db.session.execute(db.select(List).order_by(List.name)).scalars()  # (6.)  # !!
-    return render_template('lists.html', lists=lists)
-
-@app.route('/lists/<int:id>')
-def list(id):
-    list = db.session.get(List, id)  # !!
-    if list is not None:
-        return render_template('list.html', list=list)
-    else:
-        return redirect(url_for('lists'))
+    return redirect(url_for('run_insert_sample'))
 
 @app.route('/insert/sample')
 def run_insert_sample():
     insert_sample()
     return 'Database flushed and populated with some sample data.'
 
-@app.errorhandler(404)
-def http_not_found(e):
-    return render_template('404.html'), 404
+# Sample data insertion function
+def insert_sample():
+    db.session.execute(db.delete(User))
+    user1 = User(username='testuser1', email='test1@example.com', password='password123')
+    user2 = User(username='testuser2', email='test2@example.com', password='password123')
+    db.session.add_all([user1, user2])
+    db.session.commit()
 
-@app.errorhandler(500)
-def http_internal_server_error(e):
-    return render_template('500.html'), 500
-
-@app.get('/faq/<css>')
-@app.get('/faq/', defaults={'css': 'default'})
-def faq(css):
-    return render_template('faq.html', css=css)
-
-@app.get('/ex/<int:id>')
-@app.get('/ex/', defaults={'id':1})
-def ex(id):
-    if id == 1:
-        return render_template('ex1.html')
-    elif id == 2:
-        return render_template('ex2.html')
-    else:
-        abort(404)
+# Run the app
+if __name__ == '__main__':
+    app.run(debug=True)
