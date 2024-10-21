7b5296d231a177f10404b2a3bec15c48224678ed
Johann <han8@gmx.de>
2024-10-21 17:19:23 +0200

    added username again and UserMixin for validation

--- forms.py
+++ forms.py
@@ -1,10 +1,11 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import InputRequired, Length, Email, EqualTo
+from wtforms.validators import InputRequired, Length, Email, EqualTo, DataRequired
+import email_validator
 
 # Registration form
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=150)])
+    username = StringField('Username', validators=[InputRequired(), Length(min=2, max=20)])
     email = StringField('Email', validators=[InputRequired(), Email()])
     password = PasswordField('Password', validators=[InputRequired(), Length(min=6)])
     confirm_password = PasswordField('Confirm Password', validators=[InputRequired(), EqualTo('password')])
--- models.py
+++ models.py
@@ -1,7 +1,8 @@
 from db import db
+from flask_login import UserMixin
 
 # Define a simple User model for demonstration
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
